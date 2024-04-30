f44ec5132a8647bd36bf46bf1534ef71fbc173d0
Yuchan Jeon <eukkchan03@gmail.com>
2024-04-30 20:25:14 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    int solved_problems = 14; // 여기에 맞은 문제의 수를 입력하세요
+    char id[] = "eukkchan03";     // 여기에 백준 온라인 저지의 아이디를 입력하세요
+
+    printf("%d\n", solved_problems); // 맞은 문제의 수를 출력
+    printf("%s\n", id);              // 아이디를 출력
+
+    return 0;
+}
