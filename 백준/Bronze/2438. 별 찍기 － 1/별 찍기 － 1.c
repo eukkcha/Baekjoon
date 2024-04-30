c4fdc365c4eee0492ff1a6b58b5adeb9f26c570f
Yuchan Jeon <eukkchan03@gmail.com>
2024-04-30 20:32:50 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	int k = 1;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < k; j++)
+			printf("*");
+		printf("\n");
+		k++;
+	}
+
+	return 0;
+}
